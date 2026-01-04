b58eed75a0d29e944c7c3cca0c7b9b5f7cb4c10e
admin <smith.s2.pq.7@gmail.com>
2026-01-04 23:57:42 +0900

    [add] 組み込み用プログラム作成

---
+++ sync_status_embed.ino
@@ -0,0 +1,81 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#include <uri/UriBraces.h>
+#include <uri/UriRegex.h>
+
+#include "secrets.h"
+
+#define HOSTNAME "webserver"
+
+const int led_pins[] = {12, 13, 14, 15};
+
+int action = 0;
+
+unsigned long LastMeasureTime = 0;
+unsigned long Interval = 1000;
+
+void display_binary() {
+  for (int i = 0; i < sizeof(led_pins) / sizeof(led_pins[0]); i++) {
+    digitalWrite(led_pins[i], bitRead(action, i));
+  }
+}
+
+
+ESP8266WebServer server(80);
+
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, passPhrase);
+  Serial.println("");
+  WiFi.setHostname(HOSTNAME);
+
+  for (int i = 0; i < sizeof(led_pins) / sizeof(led_pins[0]); i++) {
+    pinMode(led_pins[i], OUTPUT);
+  }
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin(HOSTNAME)) { Serial.println("MDNS responder started"); }
+  MDNS.addService("http", "tcp", 80);
+
+  server.on(F("/"), []() {
+    server.send(200, "text/plain", "syncStatusServer");
+  });
+
+  server.on(UriRegex("^\\/actions\\/([0-9]+)$"), []() {
+    String str = server.pathArg(0);
+    if(str.toInt() > 15){
+      server.send(400, "text/plain", "This operation is out of scope.");
+    } else {
+      action = str.toInt();
+      server.send(200, "text/plain", "success!");
+    }
+    Serial.println(action);
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+  unsigned long CurrentTime = millis();
+  MDNS.update();
+  if ((CurrentTime - LastMeasureTime) >= Interval) {
+    display_binary();
+    LastMeasureTime = CurrentTime;
+  }
+}
